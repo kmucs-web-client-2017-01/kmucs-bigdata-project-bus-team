fc76dec4de9f89c5405160025f7be71d4d141b2a
palace2001 <palace2001@naver.com>
2017-06-01 05:28:40 +0900

    init

---
+++ src/kr/kookmin/cs/bigdata/kkp/Preprocessing.java
@@ -0,0 +1,18 @@
+package kr.kookmin.cs.bigdata.kkp;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Preprocessing {
+	private static final String[] PREWORD = {
+		"A", "a", "The", "the", "An", "an",
+		"is", "Is", "Are", "are", "am", "Am",
+		"and", "And", "or", "Or"
+	};
+	public ArrayList<String> removeArticles(String text)
+	{
+		ArrayList<String> words = new ArrayList<String>();
+		
+		return words;
+	}
+}
