06bace086f190f17960a104bc9adf69fcf6c2207
palace2001 <palace2001@naver.com>
2017-06-02 01:22:15 +0900

    removeNeedlessWord

--- src/kr/kookmin/cs/bigdata/kkp/Preprocessing.java
+++ src/kr/kookmin/cs/bigdata/kkp/Preprocessing.java
@@ -1,17 +1,33 @@
 package kr.kookmin.cs.bigdata.kkp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Preprocessing {
-	private static final String[] PREWORD = {
+
+	public static final HashMap<String, Integer> PREWORDMAP = new HashMap<String, Integer>(){{
+		for(String w : PREWORDLIST) put(w, 1); 
+    }};
+	private static final String[] PREWORDLIST = {
 		"A", "a", "The", "the", "An", "an",
 		"is", "Is", "Are", "are", "am", "Am",
-		"and", "And", "or", "Or"
+		"and", "And", "or", "Or", "to", "To",
+		"for", "For", "of", "Of", "on", "On",
+		"in", "In", "with", "With", "this", "This",
+		"that", "That" 
 	};
-	public ArrayList<String> removeArticles(String text)
+	public ArrayList<String> removeNeedlessWords(String text)
 	{
 		ArrayList<String> words = new ArrayList<String>();
+		text.replaceAll("[^a-zA-Z0-9\\s]", "");
+		String[] splitText = text.split(" ");
+		
+		for(String w : splitText) {
+			if(!PREWORDMAP.containsKey(w)) {
+				words.add(w);
+			}
+		}
 		
 		return words;
 	}
